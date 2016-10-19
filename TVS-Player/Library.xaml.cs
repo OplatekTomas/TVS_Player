b5ed343ddeb3c8ea54fb0d426911b6b88e8ca58e
TheNumerus <Petr.volf216@gmail.com>
2016-10-19 14:41:12 +0200

    some more work in library window

--- TVS-Player/Library.xaml.cs
+++ TVS-Player/Library.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,11 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace TVS_Player
 {
@@ -25,14 +28,29 @@ namespace TVS_Player
             Random r = new Random();
             for (byte i = 0; i < 10; i++)
             {
-                Rectangle rect = new Rectangle();
-                rect.Width = 180;
-                rect.Height = 200;
-                rect.Fill = new SolidColorBrush(Color.FromRgb((byte)(r.NextDouble() * 255), (byte)(r.NextDouble() * 255), (byte)(r.NextDouble() * 255)));
-                rect.Margin = new Thickness(10);
-                List.Children.Add(rect);
+                Grid folder = new Grid();
+                GenerateRectangle(out folder, r);
             }
 
+        }
+        private void GenerateRectangle(out Grid folder, Random r)
+        {
+            var xaml = XamlWriter.Save(BaseRectangle);
+            StringReader stringReader = new StringReader(xaml);
+            XmlReader xmlReader = XmlReader.Create(stringReader);
+            folder = (Grid)XamlReader.Load(xmlReader);
+            Rectangle rect = (Rectangle)folder.Children[0];
+            Color genCol = Color.FromRgb((byte)(r.NextDouble() * 255), (byte)(r.NextDouble() * 255), (byte)(r.NextDouble() * 255));
+            rect.Fill = new SolidColorBrush(genCol);
+            if (genCol.B > 128 || genCol.R > 128 || genCol.B > 128)
+            {
+                ((TextBlock)folder.Children[1]).Foreground = new SolidColorBrush(Colors.Black);
+            }
+            List.Children.Add(folder);
+        }
+        private void Quit_Event(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
