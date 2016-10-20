ccb6f9030f4c7385b547eb2ff73818f9a76d76da
Kaharonus <oplimail@gmail.com>
2016-10-20 22:17:34 +0200

    Api (poster, episode info, show info, shows with the same name info)

--- TVS-Player/Api.cs
+++ TVS-Player/Api.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace TVS_Player {
     public static class Api {
-        public static string getToken() {
+        public static void getToken() {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.thetvdb.com/login");
             request.Method = "POST";
             request.ContentType = "application/json";
@@ -20,53 +21,35 @@ namespace TVS_Player {
                     string data = "{\"apikey\": \"0E73922C4887576A\",\"username\": \"Kaharonus\",\"userkey\": \"28E2687478CA3B16\"}";
                     streamWriter.Write(data);
                 }
-            } catch (WebException) { return null; }
+            } catch (WebException) { MessageBox.Show("Make sure you are connected to the internet!"); }
             string text;
             var response = request.GetResponse();
             using (var sr = new StreamReader(response.GetResponseStream())) {
                 text = sr.ReadToEnd();
                 text = text.Remove(text.IndexOf("\"token\""), "\"token\"".Length);
                 text = text.Split('\"', '\"')[1];
-                return text;
+                Properties.Settings.Default.token = text;
+                Properties.Settings.Default.Save();
             }
         }
 
-        //jméno specifické epizody ( - S01E01 - Space Pilot 3000 )
-        public static string apiGet(int season, int episode,string id) {
+        //info o epizodě
+        public static string apiGet(int season, int episode,int id) {
             string token = Properties.Settings.Default.token;
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.thetvdb.com/series/"+id+"/episodes/query?airedSeason="+season+"airedEpisode="+episode);
             request.Method = "GET";
             request.Accept = "application/json";
             request.Headers.Add("Accept-Language", "en");
             request.Headers.Add("Authorization", "Bearer " + token);
-            string name;
             try {
                 var response = request.GetResponse();
                 using (var sr = new StreamReader(response.GetResponseStream())) {
-                    JObject parsed = JObject.Parse(sr.ReadToEnd());
-                    name = parsed["data"][0]["episodeName"].ToString();                    
+                    return sr.ReadToEnd();
                 }
             } catch (WebException) {
                 MessageBox.Show("ERROR! Please check that you are connected to the internet", "Error!");
                 return "error-api";
-                }
-
-            if (season < 10) {
-                if (episode < 10) {
-                    return " - S0" + season + "E0" + episode + name;
-                }
-                if (episode >= 10) {
-                    return " - S0" + season + "E" + episode + name;
-                }
-            }
-             if (season>=10) {
-                if (episode < 10) {
-                    return " - S" + season + "E0" + episode + name;
-                }if (episode >= 10) {
-                    return " - S" + season + "E" + episode + name;
-                }
-            }
-            return null;
+            }        
         }
 
         //Info o možných seriálech
@@ -87,8 +70,8 @@ namespace TVS_Player {
                 return "error";
             }
         }
-        //Return info about specific show
-        public static void apiGetPicture(int id) {
+        //info o specifickém seriálu
+        public static string apiGet(int id) {
             string token = Properties.Settings.Default.token;
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.thetvdb.com/series/" + id);
             request.Method = "GET";
@@ -98,12 +81,41 @@ namespace TVS_Player {
             try {
                 var response = request.GetResponse();
                 using (var sr = new StreamReader(response.GetResponseStream())) {
-                    JObject parse = JObject.Parse(sr.ReadToEnd());
+                    return sr.ReadToEnd();
 
                 }
             } catch (WebException) {
+                MessageBox.Show("ERROR! Are ya sure that you are connected to the internet?", "Error");
+                return "error";
+            }
+
+        }
+        public static void apiGetPoster(int id,string showName) {
+            string token = Properties.Settings.Default.token;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.thetvdb.com/series/" + id + "/images/query?keyType=poster");
+            request.Method = "GET";
+            request.Accept = "application/json";
+            request.Headers.Add("Accept-Language", "en");
+            request.Headers.Add("Authorization", "Bearer " + token);
+            String path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            path += "\\TVS-Player\\"+showName+ "\\Pictures" + "\\Poster\\"+showName+".jpg";
+            try {
+                var response = request.GetResponse();
+                using (var sr = new StreamReader(response.GetResponseStream())) {
+                    JObject parse = JObject.Parse(sr.ReadToEnd());              
+                    string url = "http://thetvdb.com/banners/" + parse["data"][0]["fileName"];
+                    if (!File.Exists(Path.GetDirectoryName(path))) {
+                        Directory.CreateDirectory(Path.GetDirectoryName(path));
+                    }
+                    using (WebClient client = new WebClient())
+                        client.DownloadFile(new Uri(url),path);
+                    
+                }
+            } catch (WebException) {
+                MessageBox.Show("Something");
             }
         }
+
 
     }
 }
