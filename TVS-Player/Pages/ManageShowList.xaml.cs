9b79a78ac6b3855330ea079d59e642c922527a52
Kaharonus <oplimail@gmail.com>
2016-11-04 18:24:04 +0100

    Small change - new page

---
+++ TVS-Player/Pages/ManageShowList.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace TVS_Player {
+    /// <summary>
+    /// Interaction logic for ManageShowList.xaml
+    /// </summary>
+    public partial class ManageShowList : Page {
+        string location;
+        public ManageShowList(string loc) {
+            InitializeComponent();
+            location = loc;
+            readFolders();
+        }
+        public void readFolders() {
+            List < string > subfolders = new List<String>();
+            subfolders = Directory.GetDirectories(location).ToList<string>();
+            foreach (string folder in subfolders) {
+            string show = Api.apiGet(Path.GetDirectoryName(folder));
+                if (show != null) {
+
+                }
+
+            }
+
+
+        }
+
+    }
+}
