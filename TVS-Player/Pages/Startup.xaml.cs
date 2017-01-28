f211f27500e6481b81eda388d928ab9e89c228c3
Kaharonus <oplimail@gmail.com>
2017-01-28 18:21:25 +0100

    Startup page

--- TVS-Player/Pages/Startup.xaml.cs
+++ TVS-Player/Pages/Startup.xaml.cs
@@ -5,6 +5,9 @@ using System.Windows.Forms;
 using System.IO;
 using System.Threading;
 using System.Collections.Generic;
+using Timer = System.Timers.Timer;
+using System.Timers;
+using System.Windows.Threading;
 
 namespace TVS_Player {
     /// <summary>
@@ -14,26 +17,54 @@ namespace TVS_Player {
         public Startup() {
             InitializeComponent();
             Api.getToken();
+            FadeIn();
+        }
+        Timer t = new Timer();
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e) {
+            double speed = 0.02;       
+            Dispatcher.Invoke(new Action(() => {
+                WelcomeSign.Opacity += speed;
+                if (WelcomeSign.Opacity > 1) {
+                    SetupSign.Opacity += speed;
+                }
+                if (SetupSign.Opacity > 1) {
+                    CreateSign.Opacity += speed;
+                    ImportShowBlock.Opacity += speed;
+                    AddShowBlock.Opacity += speed;
+                }
+                if (CreateSign.Opacity > 1) {
+                    t.Enabled = false;
+                }
+            }), DispatcherPriority.Send);
+        }
+        private void FadeIn() {
+            WelcomeSign.Opacity = 0;
+            SetupSign.Opacity = 0;
+            CreateSign.Opacity = 0;
+            ImportShowBlock.Opacity = 0;
+            AddShowBlock.Opacity = 0;
+            t.Interval = 16.7;
+            t.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            t.Enabled = true;
         }
 
-        private void addDB_Click(object sender, RoutedEventArgs e) {
-            /*Page showPage = new ShowList("createdb");
-            //Page showPage = new ScanLocation();
-            Window main = Window.GetWindow(this);
-            ((MainWindow)main).AddTempFrame(showPage);*/
-            
-            
-            //Renamer.RenameBatch(new List<int>(){ 121361 },new List<string>() {"D:\\Test\\S1" }, "D:\\Test\\DB");
-            //DatabaseEpisodes.readDb(121361);
-            //int k = DatabaseAPI.GetSeasons(121361);
-        }
-
-        private void importDB_Click(object sender, RoutedEventArgs e) {
+        private void ImportShowBlock_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e) {
             Page showPage = new DbLocation("import");
             Window main = Window.GetWindow(this);
             ((MainWindow)main).AddTempFrame(showPage);
         }
 
+        private void AddShowButton_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            /*Page showPage = new ShowList("createdb");
+            //Page showPage = new ScanLocation();
+            Window main = Window.GetWindow(this);
+            ((MainWindow)main).AddTempFrame(showPage);*/
 
+
+            //Renamer.RenameBatch(new List<int>(){ 121361 },new List<string>() {"D:\\Test\\S1" }, "D:\\Test\\DB");
+            //DatabaseEpisodes.readDb(121361);
+            //int k = DatabaseAPI.GetSeasons(121361);
+        }
     }
 }
