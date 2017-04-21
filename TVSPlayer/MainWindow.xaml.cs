ad6544378e02f15505cbdadb70ff901649cecf3a
Kaharonus <oplimail@gmail.com>
2017-04-21 17:51:06 +0200

    Finaly I'm doing something

--- TVSPlayer/MainWindow.xaml.cs
+++ TVSPlayer/MainWindow.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace TVSPlayer {
     /// <summary>
@@ -20,6 +23,29 @@ namespace TVSPlayer {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void SideButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            
+        }
+
+        private void MoreButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            ThemeSwitcher.SwitchTheme();
+        }
+
+        private void SearchButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            StartAnimation("ShowSearch", SearchBar);
+            StartAnimation("MoveSearchLeft", SearchButton);
+            SearchBox.Focus();
+        }
+        private void StartAnimation(string Storyboard, Grid pnl) {
+            Storyboard sb = Resources[Storyboard] as Storyboard;
+            sb.Begin(pnl);
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e) {
+            StartAnimation("HideSearch", SearchBar);
+            StartAnimation("MoveSearchRight", SearchButton);
         }
     }
 }
---
+++ TVSPlayer/Themes/ThemeSwitcher.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Color = System.Windows.Media.Color;
+using ColorConverter = System.Windows.Media.ColorConverter;
+
+namespace TVSPlayer {
+    public static class ThemeSwitcher {
+        static bool theme = false;
+        public static void SwitchTheme() {
+            Action a = () => Elapsed();
+            Thread t = new Thread(a.Invoke);
+            t.Start();
+        }
+        private static void Elapsed() {
+
+            if (theme) {
+                Application.Current.Resources["BackgroundBrush"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#242424"));
+                Application.Current.Resources["ReverseBG"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F5F5F5"));
+                Application.Current.Resources["LighterBG"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#323232"));
+                Application.Current.Resources["LightestBG"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#484848"));
+
+                LightIcons();
+                theme = false;
+            } else {
+                Application.Current.Resources["BackgroundBrush"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F5F5F5"));
+                Application.Current.Resources["ReverseBG"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#242424"));
+                Application.Current.Resources["LighterBG"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#C8C8C8"));
+                Application.Current.Resources["LightestBG"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#dcdcdc"));
+
+                DarkIcons();
+                theme = true;
+            }
+        }
+        public static void DarkIcons() {
+            Application.Current.Resources["MenuIcon"] = new BitmapImage(new Uri("pack://application:,,,/Icons/ico-menu-dark.png", UriKind.Absolute));
+            Application.Current.Resources["MoreIcon"] = new BitmapImage(new Uri("pack://application:,,,/Icons/ico-more-dark.png", UriKind.Absolute));
+            Application.Current.Resources["SearchIcon"] = new BitmapImage(new Uri("pack://application:,,,/Icons/ico-search-dark.png", UriKind.Absolute));
+        }
+        private static void LightIcons() {
+            Application.Current.Resources["MenuIcon"] = new BitmapImage(new Uri("pack://application:,,,/Icons/ico-menu-light.png", UriKind.Absolute));
+            Application.Current.Resources["MoreIcon"] = new BitmapImage(new Uri("pack://application:,,,/Icons/ico-more-light.png", UriKind.Absolute));
+            Application.Current.Resources["SearchIcon"] = new BitmapImage(new Uri("pack://application:,,,/Icons/ico-search-light.png", UriKind.Absolute));
+
+        }
+
+    }
+}
